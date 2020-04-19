2fa946f16b1c6a8e63981e31d51316b6634cc127
Carol Anderson <Carol@Carols-Mac-mini.local>
2020-04-19 17:28:29 -0600

    edit setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name=food_tools,
       version=0.0,
       description='Code for NLP projects',
       author="Carol Anderson",
-      packages=['food_tools.data_prep', 'food_tools.training'],
-      package_dir={'food_tools.data_prep': 'src/data_prep',
+      packages=['food_tools', 'food_tools.data_prep', 'food_tools.training'],
+      package_dir={'food_tools': 'src', 'food_tools.data_prep': 'src/data_prep',
             'food_tools.training': 'src/training'},
       install_requires=[
           'numpy'
