c791b313fd2285ac190ad12bfef400354340b2c4
Carol Anderson <carolmyrickanderson@gmail.com>
2020-05-03 10:30:31 -0600

    refactor evaluation to train_utils

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
 setup(name='food_tools',
-      version=0.4,
+      version=0.5,
       description='Code for NLP projects',
       author="Carol Anderson",
-      packages=['food_tools', 'food_tools.data_prep', 'food_tools.training',
-                'food_tools.evaluation'],
+      packages=['food_tools', 'food_tools.data_prep', 'food_tools.training',],
       package_dir={'food_tools': 'src', 'food_tools.data_prep': 'src/data_prep',
-            'food_tools.training': 'src/training', 'food_tools.evaluation':
-                         'src/evaluation'},
+            'food_tools.training': 'src/training',},
       install_requires=[
           'numpy', 'sklearn'
       ])
