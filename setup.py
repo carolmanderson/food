91d34f8094e3de6c1024dc53e448530ba8c48276
Carol Anderson <carolmyrickanderson@gmail.com>
2020-05-03 10:13:49 -0600

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(name='food_tools',
-      version=0.3,
+      version=0.4,
       description='Code for NLP projects',
       author="Carol Anderson",
-      packages=['food_tools', 'food_tools.data_prep', 'food_tools.training'],
+      packages=['food_tools', 'food_tools.data_prep', 'food_tools.training',
+                'food_tools.evaluation'],
       package_dir={'food_tools': 'src', 'food_tools.data_prep': 'src/data_prep',
-            'food_tools.training': 'src/training'},
+            'food_tools.training': 'src/training', 'food_tools.evaluation':
+                         'src/evalution'},
       install_requires=[
           'numpy', 'sklearn'
       ])
